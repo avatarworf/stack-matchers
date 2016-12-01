b1af0a667838ef7f3a5df9050c3108306ccf073f
Rob.Godfrey <rob.godfrey@ft.com>
2016-12-01 14:21:29 +0000

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stack_matchers',
+      version='0.0.1',
+      description='A matcher library for cloudformation stack assertions',
+      url='https://github.com/Financial-Times/stack-matchers',
+      author='Konstructor Cloud Team',
+      author_email='konstructor.cloud@ft.com',
+      license='MIT',
+      packages=['stack_matchers'],
+      zip_safe=False)
