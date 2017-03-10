d1e0f1bdc00bea9384bdeb2c1c81142bbbb133ea
Terry.Bonds <terry.bonds@ft.com>
2017-03-10 16:52:15 +0000

    Updates boto helper to deal with list_resource_stacks results that are paginated

--- stack_matchers/boto_helper.py
+++ stack_matchers/boto_helper.py
@@ -9,7 +9,7 @@ def get_stack(stack_name):
     response = client.describe_stacks(
         StackName=stack_name
     )
-    
+
     if is_empty(response['Stacks']):
         raise StackNotFoundError("Stack not found")
 
@@ -28,7 +28,18 @@ def get_stack_resources(stack_name):
     if is_empty(response['StackResourceSummaries']):
         raise StackNotFoundError("Stack not found")
 
-    return response['StackResourceSummaries']
+    full_list = []
+
+    full_list.extend(response['StackResourceSummaries'])
+
+    while 'NextToken' in response:
+        response = client.list_stack_resources(
+            StackName=stack_name,
+            NextToken=response['NextToken']
+        )
+        full_list.extend(response['StackResourceSummaries'])
+
+    return full_list
 
 def is_empty(list):
     return not list
