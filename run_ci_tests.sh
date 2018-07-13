99d2a34d71304f0f7a60ea3e671ebafad6d55121
Andrew Grant <andrew.grant@ft.com>
2018-07-13 12:08:49 +0200

    add GoCD pipeline test

---
+++ run_ci_tests.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# this is used by the aws-composer-tools pipeline to test / check coverage
+
+set -xe
+
+if [ -d venv ]; then
+    rm -rf venv
+fi
+
+virtualenv venv
+source venv/bin/activate
+set -xe
+
+curl -s https://bootstrap.pypa.io/get-pip.py > get-pip.py
+python get-pip.py
+
+pip install -r requirements.txt
+
+coverage run \
+    --source aws_composer_tools \
+    -m pytest \
+    --junitxml=tools-ci-pytest.xml \
+    tests
+coverage html \
+    --omit '*cli.py','*__init__.py' \
+    --fail-under 90
