996902b65c4e9e0a7287156bc1052c2cb3ffc32e
Abdul baki <abdul.baki@ft.com>
2018-09-21 15:45:32 +0100

    updated to use aws-composer-general package

--- run_ci_tests.sh
+++ run_ci_tests.sh
@@ -2,27 +2,32 @@
 
 # this is used by the aws-composer-tools pipeline to test / check coverage
 
-set -xe
+set -e
 
 if [ -d venv ]; then
     rm -rf venv
 fi
 
-virtualenv venv
+virtualenv -p python3.6 venv >/dev/null 2>&1
+
 source venv/bin/activate
-set -xe
 
-curl -s https://bootstrap.pypa.io/get-pip.py > get-pip.py
-python get-pip.py
+pip install --upgrade pip==10.0.1
+pip install \
+        -U \
+        -e \
+        "git+ssh://git@github.com/Financial-Times/aws-composer-pipeline-scripts-general.git@${composer_commit}#egg=aws_composer_general" \
+        "git+ssh://git@github.com/Financial-Times/aws-composer-pipeline-scripts-general.git@${composer_commit}#egg=aws_composer_general[testing]" \
+        --process-dependency-links
 
-pip install -r requirements.txt
-pip install -r requirements-test.txt
+composer run-tests tests --coverage --cov_dir stack-matchers
 
 coverage run \
     --source stack_matchers \
     -m pytest \
     --junitxml=tools-ci-pytest.xml \
     tests
+
 coverage html \
     --omit '*cli.py','*__init__.py' \
     --fail-under 60
