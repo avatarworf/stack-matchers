7c7b39e8d29ef90198300e3f382f6063f912a2f7
Andrew Grant <andrew.grant@ft.com>
2018-09-25 16:40:42 +0200

    remove import

--- tests/stack_matchers_test.py
+++ tests/stack_matchers_test.py
@@ -2,7 +2,6 @@ import pytest
 
 from hamcrest import *
 from hamcrest.core.core.isequal import equal_to
-from test_context import *
 from stack_fixtures import stack, stack_resources
 
 def test_stack_status(stack):
